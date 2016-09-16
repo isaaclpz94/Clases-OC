4a6029bfb22a2ef9f8b2a0cea80300529424854a
Isaac Mac <isaacmac@Isaacs-iMac.local>
2016-09-16 17:58:58 +0200

    Initial Commit

---
+++ Ejercicio 2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ejercicio 2
+//
+//  Created by Isaac Mac on 16/9/16.
+//  Copyright (c) 2016 Isaac Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
